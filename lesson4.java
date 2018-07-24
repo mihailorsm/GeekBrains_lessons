79be8217a9e4d78e9314eca6185f8768d55d1d32
mihailo <mihailo.rsm@gmail.com>
2018-07-24 17:26:06 +0300

    игра доделана

--- lesson4.java
+++ lesson4.java
@@ -1,8 +1,18 @@
+/**
+ * Java. Level 1. Lesson 4. Homework
+ * Tic-tac-toe in console
+ *
+ * @autor Mihail Nuzhdi
+ * @version dated Jul 24, 2018
+ */
+
 package com.company;
+
 
 import java.util.Random;
 import java.util.Scanner;
-// javadoc ??
+
+
 public class GeekLesson4 {
 
     public static int SIZE = 3;
@@ -21,7 +31,7 @@ public class GeekLesson4 {
             yourTurn();
             printMap();
             if (isWin(DOT_X)) {
-                System.out.println("Победил человек");
+                System.out.println("Вы выиграли!");
                 break;
             }
             if (isMapFull()) {
@@ -32,7 +42,7 @@ public class GeekLesson4 {
             aiTurn();
             printMap();
             if (isWin(DOT_O)) {
-                System.out.println("Победил Искуственный Интеллект");
+                System.out.println("Вы проиграли :(");
                 break;
             }
             if (isMapFull()) {
@@ -45,18 +55,18 @@ public class GeekLesson4 {
 
     public static void mapInit() {
         map = new char[SIZE + 1][SIZE + 1];
-        for (int i = 1; i < SIZE + 1; i++) {
+        for (int i = 1; i <= SIZE; i++) {
             map[0][i] = Character.forDigit(i, 10);
             map[i][0] = map[0][i];
         }
-        for (int i = 1; i < SIZE + 1; i++)
-            for (int j = 1; j < SIZE + 1; j++)
+        for (int i = 1; i <= SIZE; i++)
+            for (int j = 1; j <= SIZE; j++)
                 map[i][j] = DOT_EMPTY;
     }
 
     public static void printMap() {
-        for (int i = 0; i < SIZE + 1; i++) {
-            for (int j = 0; j < SIZE + 1; j++) {
+        for (int i = 0; i <= SIZE; i++) {
+            for (int j = 0; j <= SIZE; j++) {
                 System.out.print(map[i][j] + " ");
             }
             System.out.println();
@@ -76,70 +86,58 @@ public class GeekLesson4 {
 
     public static void aiTurn() {
         int x, y;
+        for (x = 0; x <= SIZE; x++)
+            for (y = 0; y <= SIZE; y++)
+                if (isCellValid(x,y)){
+                    map[y][x] = DOT_X;
+                    if (isWin(DOT_X)){
+                        map[y][x] = DOT_O;
+                        return;
+                    }
+                    map[y][x] = DOT_EMPTY;
+                }
         do {
-            x = rand.nextInt(SIZE+1);
-            y = rand.nextInt(SIZE+1);
+            x = rand.nextInt(SIZE + 1);
+            y = rand.nextInt(SIZE + 1);
         } while (!isCellValid(x, y));
         System.out.println("Ваш противник походил в точку " + x + " " + y);
         map[y][x] = DOT_O;
 
     }
 
+
     public static boolean isCellValid(int x, int y) {
-        if (x < 1 || x >= SIZE+1 || y < 1 || y >= SIZE+1) return false;
+        if (x < 1 || x >= SIZE + 1 || y < 1 || y >= SIZE + 1) return false;
         if (map[y][x] == DOT_EMPTY) return true;
         return false;
 
     }
 
     public static boolean isMapFull() {
-        for (int i = 1; i < SIZE+1; i++) {
-            for (int j = 1; j < SIZE+1; j++) {
+        for (int i = 1; i <= SIZE; i++) {
+            for (int j = 1; j <= SIZE; j++) {
                 if (map[i][j] == DOT_EMPTY) return false;
             }
         }
         return true;
     }
 
-    public static boolean isWin(char symb){
-        boolean flag = false;
-        for (int i = 1; i < SIZE+1; i++) {
-            for (int j = 1; j < SIZE + 1; j++) {
-                if (!(map[i][j] == symb))
-                    break;
-            }
-            flag = true;
-            if (flag)
-                break;
-        }
-        for (int i = 1; i < SIZE+1; i++) {
-            if (flag)
-                break;
-            for (int j = 1; j < SIZE + 1; j++) {
-                if (!(map[j][i] == symb))
-                    break;
-            }
-            flag = true;
-            if (flag)
-                break;
-        }
-        flag = true;
-        for (int i = 1; i < SIZE+1; i++) {
-
-                if (!(map[i][i] == symb)) {
-                    flag = false;
-                    break;
-                }
-        }
-        flag = true;
-        for (int i = 1; i < SIZE+1; i++) {
-
-                if (!(map[i][SIZE+1-i] == symb)){
-                    flag = false;
-                    break;
-                }
-                }
-        return flag;
+    public static boolean isWin(char symb) {
+        for (int y = 1; y <= SIZE; y++)
+            for (int x = 1; x <= SIZE; x++)
+                for (int dy = -1; dy < SIZE; dy++)
+                    for (int dx = -1; dx < SIZE; dx++)
+                        if (checkLine(x, y, dx, dy, symb) == SIZE)
+                            return true;
+        return false;
     }
-
+    public static int checkLine (int x, int y, int dx, int dy, char dot){
+        int count = 0;
+        if (dx ==0 && dy ==0)
+            return 0;
+        for (int i = 1, xi = x, yi =y; i <= SIZE; i++, xi += dx, yi += dy)
+            if (xi >= 1 && yi >= 1 && xi <= SIZE && yi <= SIZE && map[yi][xi] == dot)
+                count++;
+        return count;
+    }
 }
