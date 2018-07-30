e6d682feba01f737458d732b5c45ecbfe2f93c27
mihailo <mihailo.rsm@gmail.com>
2018-07-30 12:20:39 +0300

    постараюсь успеть доделать дз до занятия, был занят

---
+++ lesson6.java
@@ -0,0 +1,33 @@
+public class lesson 6 {
+
+        }
+
+        public class Animal {
+            public Animal(){
+
+            }
+            void run(){
+
+            }
+
+            void fly(){
+
+            }
+
+            void swim(){
+
+            }
+        }
+
+        public class Cat extends Animal {
+
+        }
+
+        public class Dog extends Animal {
+
+        }
+
+public class MainClass {
+    public static void main(String[] args) {
+        
+    }
