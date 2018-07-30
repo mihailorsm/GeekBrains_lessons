85aeed9c5e6e9fb5f76442592336ee402732ef11
mihailo <mihailo.rsm@gmail.com>
2018-07-30 12:45:26 +0300

    сделал

--- lesson6.java
+++ lesson6.java
@@ -6,28 +6,77 @@ public class lesson 6 {
             public Animal(){
 
             }
-            void run(){
-
+           public void run(float distance){
+                try {
+                    if (distanc <= 500)
+                        System.out.println("run: " + true);
+                    else
+                        System.out.println("run: " + false);
+                } catch (new IllegalArgumeentException e)
+                {}
             }
 
-            void fly(){
-
+           public void jump(float hight){
+                try {
+                    if (hight <= 0.5)
+                        System.out.println("run: " + true);
+                    else
+                        System.out.println("run: " + false);
+                } catch (new IllegalArgumeentException e)
+                {}
             }
 
-            void swim(){
-
+           public void swim(float distance){
+                try {
+                    if (distanc <= 10)
+                        System.out.println("run: " + true);
+                    else
+                        System.out.println("run: " + false);
+                } catch (new IllegalArgumeentException e)
+                {}
             }
         }
+public class Cat extends Animal {
+    @java.lang.Override
+    public void run(float distance) {
+        super.run(distance);
+    }
 
-        public class Cat extends Animal {
+    @java.lang.Override
+    public void jump(float hight) {
+        super.jump(hight);
+    }
 
-        }
+    @java.lang.Override
+    public void swim(float distance) {
+        System.out.println("run: " + false);
+    }
+}
 
-        public class Dog extends Animal {
+public class Dog extends Animal {
+    @java.lang.Override
+    public void run(float distance) {
+        super.run(distance);
+    }
 
-        }
+    @java.lang.Override
+    public void jump(float hight) {
+        super.jump(hight);
+    }
+
+    @java.lang.Override
+    public void swim(float distance) {
+        return super.swim(distance);
+    }
+}
+
+
 
 public class MainClass {
     public static void main(String[] args) {
-        
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+    }
+
     }
